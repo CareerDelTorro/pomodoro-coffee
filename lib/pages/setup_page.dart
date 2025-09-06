9a28a2580a5c0a74424ddffcc1f08149ec9c9629
Sergey Germogentov <sergey.grmg@gmail.com>
2025-09-06 22:42:15 +1000

    add a slider

--- lib/pages/setup_page.dart
+++ lib/pages/setup_page.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
-class SetupPage extends StatelessWidget {
+class SetupPage extends StatefulWidget {
   const SetupPage({super.key});
+
+  @override
+  State<SetupPage> createState() => _SetupPageState();
+}
+
+class _SetupPageState extends State<SetupPage> {
+  int workDuration = 25;
+  int breakDuration = 5;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.pushNamed(context, '/session'),
-          child: Text("Start"),
+        child: Column(
+          children: [
+            Text('Set Work Duration (minutes)'),
+            Slider(
+              min: 10,
+              max: 60,
+              divisions: 10,
+              value: workDuration.toDouble(),
+              onChanged: (newValue) => {
+                workDuration = newValue.toInt(),
+                setState(() {}),
+              },
+            ),
+            SizedBox(height: 20),
+            Text('Work Duration: $workDuration minutes'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/session'),
+              child: Text("Start"),
+            ),
+          ],
         ),
       ),
     );

71b53dcd6bbf39da33718abc130275204daac1f1
Sergey Germogentov <sergey.grmg@gmail.com>
2025-09-07 11:37:25 +1000

    add total session num in the results page

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_coffee/app_state.dart';
+import 'package:provider/provider.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({super.key});
@@ -11,7 +13,9 @@ class ResultsPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Results Page'),
+            Text(
+              'Congratulations! You completed ${Provider.of<AppState>(context, listen: false).numSessionsTotal} sessions.',
+            ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
